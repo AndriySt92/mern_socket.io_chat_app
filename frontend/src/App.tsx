d1f40c3b70bc157a63d16fc68c2f290a3f58f415
AndriySt92 <andriyua-1992@mail.ru>
2024-05-31 21:32:19 +0300

    Add Signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Toaster } from "react-hot-toast"
 import Login from "./pages/login/Login"
+import Signup from "./pages/signup/Signup"
 
 function App() {
   return (
     <div className='p-4 h-screen flex justify-center items-center'>
-      <Login />
+      <Signup />
       <Toaster/>
     </div>
   )
--- frontend/src/interfaces/userInterfaces.ts
+++ frontend/src/interfaces/userInterfaces.ts
@@ -1,6 +1,14 @@
 interface IUser {
-    _id: string
-    fullName: string
-    username: string
-    profilePic: string
+  _id: string
+  fullName: string
+  username: string
+  profilePic: string
+}
+
+export interface ISignup {
+  fullName: string
+  username: string
+  password: string
+  confirmPassword: string
+  gender: string
 }
