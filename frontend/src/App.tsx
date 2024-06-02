e1b9c3b88d1d67dbe4ccef6d5dd3d9f31d80421e
AndriySt92 <andriyua-1992@mail.ru>
2024-06-02 22:04:38 +0300

    Add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import { Toaster } from "react-hot-toast"
-import Login from "./pages/login/Login"
-import Signup from "./pages/signup/Signup"
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { useAuthContext } from './context/AuthContext';
+import Home from './pages/home/Home';
 
 function App() {
+  const { authUser } = useAuthContext();
+
   return (
-    <div className='p-4 h-screen flex justify-center items-center'>
-      <Signup />
-      <Toaster/>
+    <div className="p-4 h-screen flex justify-center items-center">
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={'/login'} />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
+      </Routes>
+      <Toaster />
     </div>
   )
 }
